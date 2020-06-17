bcc4c8781c273fe32f8254f1abd68c382393bdde
Igor Kuznietsov <ultexplorer@gmail.com>
2020-06-17 23:09:18 +0300

    Не работает код...

--- TimerTest/src/ua/com/ultexplorer/timertest/TimerTest.java
+++ TimerTest/src/ua/com/ultexplorer/timertest/TimerTest.java
@@ -1,10 +1,34 @@
 package ua.com.ultexplorer.timertest;
 
-public class TimerTest {
+import java.awt.*;
+import java.awt.event.*;
+import java.lang.invoke.VarHandle;
+import java.time.*;
+import javax.swing.*;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class TimerTest 
+{
+	public static void main(String[] args) 
+	{
+		var listener = new TimePrinter();
+		var timer = new Timer(1000, listener);
+		timer.restart();
+		
+		//JOptionPane.showMessageDialog(null, "Quit program?");
+		
+		System.exit(0);
 
 	}
 
+}
+
+class TimePrinter implements ActionListener 
+{
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		System.out.println("At the tone, the time is "
+				+ Instant.ofEpochMilli(event.getWhen()));
+		Toolkit.getDefaultToolkit().beep();
+	}
+	
 }
